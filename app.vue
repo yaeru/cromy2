244502277fbc01e9574367d49f55bc6b2083387e
Yael Duckwen <yaelduckwen@gmail.com>
2023-11-09 17:24:32 -0300

    Init

---
+++ app.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+	const supabase = useSupabaseClient()
+
+	const { data: cards } = await useAsyncData(async () => {
+		const { data } = await supabase.from('cards').select('*')
+		return data
+	})
+</script>
+
+<template>
+	<NuxtLayout :name="layout">
+		<div>
+			<h1>
+				Test
+			</h1>
+			<ul>
+				<li v-for="card in cards" :key="card.id">
+					<h3>
+						{{ card.title }}
+					</h3>
+					<p>
+						{{ card.description }}
+					</p>
+				</li>
+			</ul>
+			<pre>
+				{{ cards }}
+			</pre>
+		</div>
+	</NuxtLayout>
+</template>
