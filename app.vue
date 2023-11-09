345f9d99a5a5e1ff51257b2f3830764a51976bf4
Yael Duckwen <yaelduckwen@gmail.com>
2023-11-09 18:01:14 -0300

    add uikit

--- app.vue
+++ app.vue
@@ -9,23 +9,34 @@
 
 <template>
 	<NuxtLayout :name="layout">
-		<div>
-			<h1>
-				Test 2
-			</h1>
-			<ul>
-				<li v-for="card in cards" :key="card.id">
-					<h3>
-						{{ card.title }}
-					</h3>
-					<p>
-						{{ card.description }}
-					</p>
-				</li>
-			</ul>
-			<pre>
-				{{ cards }}
-			</pre>
+		<div class="uk-container">
+			<section class="uk-section">
+				<h1>
+					Test 2
+				</h1>
+				<ul class="uk-grid uk-child-width-1-2 uk-grid uk-child-width-1-3@m uk-grid-match" uk-grid>
+					<li v-for="card in cards" :key="card.id">
+						<div class="uk-card uk-card-default uk-card-small">
+							<div class="uk-card-header">
+								<h3 class="uk-card-title uk-margin-remove-bottom">
+									{{ card.title }}
+								</h3>
+								<p class="uk-text-meta uk-margin-remove-top">
+									{{ card.created_at }}
+								</p>
+							</div>
+							<div class="uk-card-body">
+								<p>
+									{{ card.description }}
+								</p>
+							</div>
+						</div>
+					</li>
+				</ul>
+				<pre>
+					{{ cards }}
+				</pre>
+			</section>
 		</div>
 	</NuxtLayout>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase'],
+  modules: ['@nuxtjs/supabase', '@samk-dev/nuxt-uikit3'],
 
   supabase: {
     redirect: false
