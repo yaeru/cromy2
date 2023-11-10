e0edeee0a61bbf881c9db51669b2b832e7a305c8
Yael Duckwen <yaelduckwen@gmail.com>
2023-11-10 19:10:32 -0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 		<h1>Welcome to the homepage</h1>
-		<AppAlert state="danger">
-			This is an auto-imported component
-		</AppAlert>
+		<NuxtLink to="/cards" class="uk-button uk-button-primary uk-button-large">
+			Ir a Cartas
+		</NuxtLink>
 </template>
